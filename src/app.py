96474abff2b5f1ca8e8e4ea6b245a8e2094cdc81
cornzz <39997278+cornzz@users.noreply.github.com>
2024-09-08 22:08:31 +0200

    Gh logo

--- src/app.py
+++ src/app.py
@@ -47,6 +47,7 @@ LLM_ENDPOINT, LLM_TOKEN, LLM_LIST = get_api_info()
 APP_PATH = os.getenv("APP_PATH") or ""
 MPS_AVAILABLE = torch.backends.mps.is_available()
 CUDA_AVAILABLE = torch.cuda.is_available()
+GH_LOGO = """<svg viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/></svg>"""
 
 app = FastAPI(openapi_url="", root_path=APP_PATH)
 llm_lingua = PromptCompressor(
@@ -200,7 +201,7 @@ def run_demo(
 with gr.Blocks(
     title="LLMLingua-2 Demo", css=os.path.join(BASE_DIR, "app.css"), js=os.path.join(BASE_DIR, "app.js")
 ) as demo:
-    gr.Markdown("# Prompt Compression Demo")
+    gr.Markdown(f"# Prompt Compression Demo <a class=\"source\" href=\"https://github.com/cornzz/llmlingua-demo\" target=\"_blank\">{GH_LOGO}</span>")
     with gr.Accordion("About this demo (please read):", open=False, elem_classes="accordion"):
         gr.Markdown(
             "Your prompt is sent to a target LLM for completion, both in its uncompressed form and compressed using [LLMLingua-2](https://llmlingua.com/llmlingua2.html). Evaluate the responses and select which one you prefer."
@@ -310,7 +311,6 @@ with gr.Blocks(
         type="index",
         elem_id="examples",
     )
-    gr.Markdown("Source: [GitHub](https://github.com/cornzz/llmlingua-demo)", elem_classes="center")
 
     # Event handlers
     prompt.change(activate_button, inputs=prompt, outputs=submit)
